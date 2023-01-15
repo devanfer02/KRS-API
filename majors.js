a79ddb6b54c1c0dea8921dbc287368f51140fb85
Devan F <116249842+devanfer02@users.noreply.github.com>
2023-01-15 09:22:16 +0700

    Delete majors.js

--- majors.js
+++
@@ -1,9 +0,0 @@
-const majors = [
-    'Teknik Informatika',
-    'Teknik Komputer',
-    'Sistem Informasi',
-    'Pendidikan Teknologi Informasi',
-    'Teknologi Informasi'
-]
-
-module.exports = majors
