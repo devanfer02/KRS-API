e6e0c7de8275e1037e0ccf4e8e16cd70d347a8c6
Devan F <116249842+devanfer02@users.noreply.github.com>
2023-01-15 08:15:02 +0700

    Update function.js

--- src/function.js
+++ src/function.js
@@ -6,7 +6,7 @@ const indexCourse = (nim) => {
     const currentMonth = currentTime.getMonth() + 1
     const nimYear = nim.substring(0, 2)
     const prodiNumber = nim.substring(6, 7)
-    const indexSpace = (prodiNumber < 5 ? (prodiNumber - 2) * 5 : (prodiNumber - 3) * 5)
+    const indexSpace = (prodiNumber < 5 ? (prodiNumber - 2) * 8 : (prodiNumber - 3) * 8)
     if (currentMonth < 6) {
         return (2 * (currentYear - nimYear) - 1) + indexSpace
     } else {
