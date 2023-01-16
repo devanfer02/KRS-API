5f70634cdd6971a79975586e527f96a10e451ea0
Devan F <116249842+devanfer02@users.noreply.github.com>
2023-01-16 07:04:49 +0700

    Add files via upload

---
+++ src/course/PendidikanTeknologInformasi.js
@@ -0,0 +1,76 @@
+const ptiSem1 = {
+    'Pemrograman Dasar': 4,
+    'Perkembangan dan Peserta Didik': 3,
+    'Filsafat dan Ilmu Pendidikan': 3,
+    'Pengenalan Keilmuan Komputer': 2,
+    'Bahasa Inggris': 2,
+    'Arsitektur dan Organisasi Komputer': 3,
+    'Agama': 2
+}
+
+const ptiSem2 = {
+    'Matematika Komputasi': 3,
+    'Pemrograman Berorientasi Objek': 3,
+    'Statistika Dasar': 3,
+    'Bahasa Indonesia': 2,
+    'Pancasila': 2,
+    'Manajemen Kurikulum dan Bidang Studi': 3,
+    'Teori Belajar dan Pembelajaran': 3,
+    'Sistem Operasi': 3
+}
+
+const ptiSem3 = {
+    'Algoritma dan Struktur Data': 3,
+    'Pengantar Sains Data': 3,
+    'Desain Basis Data dan SQL': 3,
+    'Perencanaan dan Desain Pembelajaran': 3,
+    'Kewarganegaraan': 2,
+    'Desain Antarmuka Pengguna': 3,
+    'Sistem Informasi dan Proses Bisnis': 2,
+    'Penilaian dan Evaluasi Hasil Belajar': 3
+}
+
+const ptiSem4 = {
+    'Jaringan Komputer': 3,
+    'Pemrograman Web': 3,
+    'Analisis Sistem Informasi dan Design': 3,
+    'Kecerdasan Buatan': 3,
+    'Manajemen Pengetahuan dan Sumber Belajar': 3,
+    'Etika Profesi': 2,
+    'Instruksional Desain Interaksi Media': 3
+}
+
+const ptiSem5 = {
+    'Etika Profesi': 2,
+    'Metodologi Penelitian dan Penulisan Ilmiah': 3,
+    'Penambangan Data': 3,
+    'Enterpreneurship': 2,
+    'Implementasi dan Pengujian Sistem Informasi': 3,
+    'Microteaching dan Manajemen Kelas': 3,
+    'Pengenalan Lapangan Persekolahan': 1
+
+}
+
+const ptiSem6 = {
+
+}
+
+const ptiSem7 = {
+    'Pengenalan Lapangan Persekolahan': 3,
+    'Praktek Lapangan': 4
+}
+
+const ptiSem8 = {
+
+}
+
+module.exports = {
+    ptiSem1,
+    ptiSem2,
+    ptiSem3,
+    ptiSem4,
+    ptiSem5,
+    ptiSem6,
+    ptiSem7,
+    ptiSem8
+}
---
+++ src/course/TeknologiInformasi.js
@@ -0,0 +1,72 @@
+const tiSem1 = {
+    'Bahasa Inggris': 2,
+    'Arsitektur dan Organisasi Komputer': 3,
+    'Pemrogramn Dasar': 4,
+    'Pengantar Keilmuan Komputer': 2,
+    'Matematika Komputasi': 3,
+    'Agama': 2,
+    'Pancasila': 2
+}
+
+const tiSem2 = {
+    'Pemrograman Lanjut': 5,
+    'Kewarganegaarn': 2,
+    'Sistem Basis Data': 4,
+    'Dasar Desain Antarmuka Pengguna': 3,
+    'Dasar Pengembangan Sistem Informasi': 2,
+    'Pengantar Sistem Operasi': 2,
+    'Bahasa Indonesia': 2
+}
+
+const tiSem3 = {
+    'Statitiska': 3,
+    'Pengembangan Aplikasi Web': 4,
+    'Desain dan Analisis Algoritma': 3,
+    'Analisis dan Desain Sistem Informasi': 4,
+    'Jaringan Komputer Dasar': 3,
+    'Pemrograman Basis Data': 3
+}
+
+const tiSem4 = {
+    'Pengembangan Aplikasi Mobile': 4,
+    'Tata Kelola Teknologi Informasi': 3,
+    'Implementasi dan Evaluasi Sistem Informasi': 3,
+    'Administrasi Sistem': 3,
+    'Arsitektur dan Protokol Internet': 3,
+    'Teknologi Terintegrasi Sistem Pembelajaran Mesin': 2,
+    'Keamanan Informasi': 3
+}
+
+const tiSem5 = {
+    'Etika Profesi': 2,
+    'Metodologi Penelitian dan Penulisan Ilmiah': 3,
+    'Rekayasa Sistem Komputer': 3,
+    'Keamanan Jaringan': 3,
+    'Administrasi Basis Data': 4,
+    'Pemrograman Integratif': 4,
+    'Kewirausahaan': 2,
+    'Data Warehouse': 3
+}
+
+const tiSem6 = {
+
+}
+
+const tiSem7 = {
+
+}
+
+const tiSem8 = {
+
+}
+
+module.exports = {
+    tiSem1,
+    tiSem2,
+    tiSem3,
+    tiSem4,
+    tiSem5,
+    tiSem6,
+    tiSem7,
+    tiSem8
+}
